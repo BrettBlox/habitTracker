f9a8e16a28924f51acb3d4108253bf453c5a6606
Brett Bloxom <brettbloxom@gmail.com>
2019-11-03 21:13:05 -0700

    styled jsx

--- components/Habit.js
+++ components/Habit.js
@@ -1,13 +1,13 @@
-import HabitButton from './HabitButton'
+import HabitButton from './HabitButton';
 
-const colors = ['#718096', '#F56565', '#F6E05E', '#68D391', '#63B3ED']
+const colors = ['#718096', '#F56565', '#F6E05E', '#68D391', '#63B3ED'];
 
 const Habit = ({ habit, index }) => {
-  const dates = getLast5Days()
+  const dates = getLast5Days();
   return (
     <article>
       <h3 style={{ borderColor: colors[index] }}>{habit}</h3>
-      <div className='buttons'>
+      <div className="buttons">
         {dates.map(date => (
           <HabitButton key={date.getTime()} date={date} />
         ))}
@@ -31,16 +31,16 @@ const Habit = ({ habit, index }) => {
         `}
       </style>
     </article>
-  )
-}
+  );
+};
 
 const getLast5Days = () => {
   const dates = '01234'.split('').map(day => {
-    const tempDate = new Date()
-    tempDate.setDate(tempDate.getDate() - day)
-    return tempDate
-  })
-  return dates
-}
+    const tempDate = new Date();
+    tempDate.setDate(tempDate.getDate() - day);
+    return tempDate;
+  });
+  return dates;
+};
 
-export default Habit
+export default Habit;
