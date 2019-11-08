34fd3358e1bf984699c1b5ec65e6d809e5ffdddd
Brett Bloxom <brettbloxom@gmail.com>
2019-11-07 20:04:16 -0700

    habit margin bottom

--- components/Habit.js
+++ components/Habit.js
@@ -1,13 +1,13 @@
-import HabitButton from './HabitButton';
+import HabitButton from './HabitButton'
 
-const colors = ['#718096', '#F56565', '#F6E05E', '#68D391', '#63B3ED'];
+const colors = ['#718096', '#F56565', '#F6E05E', '#68D391', '#63B3ED']
 
 const Habit = ({ habit, index }) => {
-  const dates = getLast5Days();
+  const dates = getLast5Days()
   return (
     <article>
       <h3 style={{ borderColor: colors[index] }}>{habit.name}</h3>
-      <div className="buttons">
+      <div className='buttons'>
         {dates.map(date => (
           <HabitButton
             key={date.getTime()}
@@ -23,6 +23,7 @@ const Habit = ({ habit, index }) => {
             padding: 20px;
             border-radius: 15px;
             box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.1);
+            margin-bottom: 10px;
           }
 
           h3 {
@@ -36,16 +37,16 @@ const Habit = ({ habit, index }) => {
         `}
       </style>
     </article>
-  );
-};
+  )
+}
 
 const getLast5Days = () => {
   const dates = '01234'.split('').map(day => {
-    const tempDate = new Date();
-    tempDate.setDate(tempDate.getDate() - day);
-    return tempDate;
-  });
-  return dates;
-};
+    const tempDate = new Date()
+    tempDate.setDate(tempDate.getDate() - day)
+    return tempDate
+  })
+  return dates
+}
 
-export default Habit;
+export default Habit
